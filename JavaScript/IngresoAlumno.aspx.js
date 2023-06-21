6924768bdaa6798d3617ae8a980a9c16e027f809
Horwell <horwellmv@gmail.com>
2023-06-20 21:51:12 -0300

    Agrega hojas de estilo y funciones de Js

--- JavaScript/IngresoAlumno.aspx.js
+++ JavaScript/IngresoAlumno.aspx.js
@@ -1,2 +1,33 @@
 ﻿
 //Desarrollar aqui las validaciones para Form IngresoAlumno.aspx
+
+
+function validarCampos() {
+    var apellido = document.getElementById('apellidoTextbox').value;
+    var nombre = document.getElementById('nombreTextbox').value;
+    var turno = document.getElementById('turnoDropList').value;
+    var email = document.getElementById('emailTextbox').value;
+
+    // Validar campos vacíos
+    if (apellido === '' || nombre === '' || turno === 'Elije turno' || email === '') {
+        alert('Todos los campos son obligatorios');
+        return false;
+    }
+
+    // Validar nombre y apellido solo con letras
+    var letras = /^[A-Za-z]+$/;
+    if (!apellido.match(letras) || !nombre.match(letras)) {
+        alert('El nombre y el apellido solo pueden contener letras');
+        return false;
+    }
+
+    // Validar formato de correo electrónico
+    var emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    if (!email.match(emailRegex)) {
+        alert('El formato del correo electrónico no es válido');
+        return false;
+    }
+
+    // Si todas las validaciones pasan, se puede enviar el formulario
+    return true;
+}
--- JavaScript/MostrarDato.js
+++ JavaScript/MostrarDato.js
@@ -1,2 +1,9 @@
 ﻿
 //Desarrollar aqui las validaciones para Form MostarDato.aspx
+function validarId() {
+    var id = document.getElementById('txtId').value;
+    if (id === '' || !/^\d+$/.test(id)) {
+        alert('El Id del comentario no puede estar vacio');
+        return false;
+    }
+}
